6f25b9193779d4e204533fbf9608116c4bfe8a46
AndyGar04 <andygarciaprost2004@gmail.com>
2025-03-13 15:18:20 -0300

    Actualizacion: Entrega

--- AppCoder/restaurante/urls.py
+++ AppCoder/restaurante/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "restaurante"
 
 urlpatterns = [
-    path("restaurante/gerente_list", views.gerente_list, name="gerente_list"),
-    path("restaurante/pedido_list", views.pedido_list, name="pedido_list"),
-    path("restaurante/empleado_list", views.empleado_list, name="empleado_list"),
+    path("gerente_list/", views.gerente_list, name="gerente_list"),
+    path("pedido_list/", views.pedido_list, name="pedido_list"),
+    path("empleado_list/", views.empleado_list, name="empleado_list"),
 ]
